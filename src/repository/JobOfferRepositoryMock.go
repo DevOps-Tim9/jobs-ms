ff86c48219f00703da3a0dbcbd203a4da067202f
Jelena Cupac <52001925+jelenac11@users.noreply.github.com>
2022-06-08 19:44:43 +0200

    feat: search job offers (#2)
    * feat: implement job offer creation
    
    * test: add unit test
    
    * feat: implement search job offers
    
    * test: add unit tests
    
    * refactor: remove duplicated import

--- src/repository/JobOfferRepositoryMock.go
+++ src/repository/JobOfferRepositoryMock.go
@@ -17,3 +17,27 @@ func (repo *JobOfferRepositoryMock) Add(offer model.JobOffer) (model.JobOffer, e
 	}
 	return args.Get(0).(model.JobOffer), args.Get(1).(error)
 }
+
+func (repo *JobOfferRepositoryMock) GetByCompany(id int) ([]*model.JobOffer, error) {
+	args := repo.Called(id)
+	if args.Get(1) == nil {
+		return args.Get(0).([]*model.JobOffer), nil
+	}
+	return args.Get(0).([]*model.JobOffer), args.Get(1).(error)
+}
+
+func (repo *JobOfferRepositoryMock) GetAll() ([]*model.JobOffer, error) {
+	args := repo.Called()
+	if args.Get(1) == nil {
+		return args.Get(0).([]*model.JobOffer), nil
+	}
+	return args.Get(0).([]*model.JobOffer), args.Get(1).(error)
+}
+
+func (repo *JobOfferRepositoryMock) Search(param string) ([]*model.JobOffer, error) {
+	args := repo.Called(param)
+	if args.Get(1) == nil {
+		return args.Get(0).([]*model.JobOffer), nil
+	}
+	return args.Get(0).([]*model.JobOffer), args.Get(1).(error)
+}
